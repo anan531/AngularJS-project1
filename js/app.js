30cbda4f5da21ad41358b0116f69a59c67d29d6b
anan531 <ananyat612@gmail.com>
2026-07-21 12:18:46 +0530

    updated

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var app=angular.module('campus360',[]);
+
+
+app.controller("dashboardcontroller",function($scope){
+
+    $scope.collegename="FISAT";
+    $scope.Name="Ananya T";
+    $scope.email="ananyat@gmail.com";
+    $scope.regno="007/MCA";
+    $scope.dep="CA";
+    $scope.course="MCA";
+    $scope.totalstudents="0";
+
+    console.log("Dashboard Contoller Loaded Successfully");
+    $scope.students=[
+
+        "Arya","Jyothika","Archana","Akshara"
+
+    ];
+
+    $scope.addstudent=function(){
+
+        $scope.totalstudents++;
+
+    };
+
+
+
+});
